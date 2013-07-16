7cc6f83e6108c1739110f9e59752fea7dfe1bc1a
sunhaowen <sunhaowen123@gmail.com>
2013-07-16 17:12:10 +0800

    sunhaowen

--- tool/multiRequest.py
+++ tool/multiRequest.py
@@ -9,13 +9,16 @@ import logging
 import time
 import json
 
-logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(message)s', )
+
+logger = logging.getLogger("work")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 
 def here():
     print("PrimeMusic")
 
 class counter(object) :
-    def __int__(self, start = 0):
+    def __init__(self, start = 0):
         self.lock = threading.Lock()
         self.value = start
 
@@ -34,15 +37,15 @@ def send_get_requests(index, raw_url, error_count) :
         result_list = result.json()
     except Exception, e :
         error_count.increment()
-        logging.debug("%d error %s %s" % (index, e, raw_url))
+        logger.debug("%d error %s %s" % (index, e, raw_url))
         return False
 
     if type(result_list) == type({}) :
         error_count.increment()
-        logging.debug("%d error %s %s" % (index, result_list["error_code"], raw_url))
+        logger.debug("%d error %s %s" % (index, result_list["error_code"], raw_url))
         return False
 
-    logging.debug("%d right %s" % (index, " ".join("%s" % dir["url"] for dir in result_list)))
+    logger.debug("%d right %s" % (index, " ".join("%s" % dir["url"] for dir in result_list)))
     return True
 
 
@@ -53,11 +56,13 @@ if __name__ == "__main__":
     frequency = 5
     for index, raw_url in enumerate(raw_url_list) :
         get_request = threading.Thread(name=index, target=send_get_requests, args=(index, raw_url, error_count))
-        if index % frequency == 0 :
-            time.sleep(1.0)
+        get_request.start()
+        get_request.join()
 
     main_thread = threading.currentThread()
     for t in threading.enumerate() :
         if t is main_thread :
             continue
         t.join()
+
+    print(error_count.value, len(raw_url_list))
