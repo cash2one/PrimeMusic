a2486ab742aa98b55ca5c514250d4d4fb6bd9733
sunhaowen <sunhaowen123@gmail.com>
2014-01-09 16:02:02 +0800

    cluster

--- tool/novel/cluster/check_change_book_name.py
+++ tool/novel/cluster/check_change_book_name.py
@@ -27,10 +27,43 @@ def get_dir_id_list():
         for (dir_id, dir_url, rid) in cursor.fetchall():
             dir_id_list.append((dir_id, dir_url, rid))
     cursor.close()
+    conn.close()
 
     file_handler = open("./dir_id_list.txt", "w")
     for (dir_id, dir_url, rid) in dir_id_list:
         file_handler.write("%d  %s  %d\n" % (dir_id, dir_url, rid))
+    file_handler.close()
+
+def check_diff_rid():
+    dir_id_list = []
+    file_handler = open("./dir_id_list.txt", "r")
+    for line in file_handler.readlines():
+        (dir_id, dir_url, rid) = line.split()
+        dir_id_list.append((dir_id, dir_url, rid))
+    file_handler.close()
+
+    conn = MySQLdb.connect(
+        host = "10.46.7.171",
+        port = 4198,
+        user = "wise_novelclu_w",
+        passwd = "C9l3U4n6M2e1",
+        db = "novels_new")
+    cursor = conn.cursor()
+
+    diff_list = []
+    for (dir_id, dir_url, rid) in dir_id_list:
+        table_id = dir_id % 10
+        sql = "SELECT rid FROM dir_rid_info%d WHERE dir_id = %d" % (table_id, dir_id)
+        cursor.execute(sql)
+        (new_rid, ) = cursor.fetchone()
+        if rid != new_rid:
+            diff_list.append((dir_id, dir_url, rid, new_rid))
+    cursor.close()
+    conn.close()
+
+    file_handler = open("./diff_list.txt", "w")
+    for (dir_id, dir_url, rid, new_rid) in diff_list:
+        file_handler.write("%d  %s  %d  %d\n" % (dir_id, dir_url, rid, new_rid))
     file_handler.close()
 
 if __name__ == '__main__':
