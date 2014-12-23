14a3613715cc34f0a541e058d3c00d0d48a5a9eb
sunhaowen <sunhaowen123@gmai.com>
2014-12-23 22:30:26 +0800

    test

---
+++ tool/search/video_query.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:GBK
+
+__author__ = 'sunhaowen'
+__date__ = '2014-12-23 18:24'
+
+
+def here():
+    print('PrimeMusic')
+
+
+if __name__ == '__main__':
+    here()    
+
+
+
+
+
+
+
