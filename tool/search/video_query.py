4b1c60f0fc9da6aad9a783f59795d2cd07fe5d8a
sunhaowen <sunhaowen123@gmai.com>
2014-12-24 17:41:32 +0800

    test

--- tool/search/video_query.py
+++ tool/search/video_query.py
@@ -5,10 +5,19 @@ __author__ = 'sunhaowen'
 __date__ = '2014-12-23 18:24'
 
 import requests
-from bs4 import BeautifulSoup
+import json
 
 def here():
     print('PrimeMusic')
+
+
+def check_query(word):
+    """
+    :param word:
+    :return:
+    """
+    src = 'https://m.baidu.com/s?word={0}'.format(word)
+    r = requests.get()
 
 
 if __name__ == '__main__':
