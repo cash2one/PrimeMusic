28291b216fcb7fcc5a0f19bffa1793db566d9ee5
sunhaowen <sunhaowen123@gmai.com>
2015-01-06 14:19:06 +0800

    test

---
+++ tool/DA/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:GBK
+
+__author__ = 'sunhaowen'
+__date__ = '2015-01-06 14:16'
+
+
+def here():
+    print('PrimeMusic')
+
+
+if __name__ == '__main__':
+    here()    
+
+
+
+
+
+
+
