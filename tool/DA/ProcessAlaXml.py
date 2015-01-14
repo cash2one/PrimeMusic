8ef364fdce846fa1b9eb32615162d023af8b5ba8
sunhaowen <sunhaowen123@gmai.com>
2015-01-14 11:23:49 +0800

    Merge branch 'master' of github.com:sunhaowen/PrimeMusic
    Conflicts:
    	tool/multiRequest.py

--- tool/DA/ProcessAlaXml.py
+++ tool/DA/ProcessAlaXml.py
@@ -18,6 +18,7 @@ def get_xml_key_list(src):
     try:
         response = requests.get(src)
         tree = ElementTree.fromstring(response.content)
+        print('src: {0}'.format(src))
     except Exception, e:
         return []
 
@@ -35,6 +36,7 @@ def process_single_site(src):
     try:
         response = requests.get(src)
         tree = ElementTree.fromstring(response.content)
+        print('src: {0}'.format(src))
     except Exception, e:
         return []
 
@@ -50,9 +52,19 @@ def process_single_site(src):
 
 
 if __name__ == '__main__':
-    srcid = 20426
-    src = 'http://yf-imps-bs07-09.yf01.baidu.com:8888/index.xml'
-    key_list = process_single_site(src)
-    print('srcid: {0}, src: {1}'.format(srcid, src))
-    for key in key_list:
-        print(key)
+
+    source_list = [
+        (20426, 'http://yf-imps-bs07-09.yf01.baidu.com:8888/index.xml'),
+        (16325, 'http://cq02-mco-wise-build00.cq02.baidu.com:8091/xml_build/jiajintao/people_zhixin/people.xml'),
+        (11198, 'http://ald.app111.com/precise_strong.xml'),
+        (11235, 'http://ald.app111.com/addressing.xml'),
+        (12064, 'http://ald.app111.com/function_weak.xml'),
+        (11312, 'http://ald.app111.com/function_strong.xml')
+    ]
+    for (srcid, src) in source_list:
+        print('srcid: {0}, src: {1}'.format(srcid, src))
+        result = process_single_site(src)
+        file = open('./{0}.txt'.format(src), 'w')
+        for key in result:
+            file.write('{0}\n'.format(key.encode('GBK')))
+        file.close()
