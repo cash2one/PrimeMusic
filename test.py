623ac91d04966d9aaaa8bf9533cc0ed8e09f2d83
sunhaowen <sunhaowen123@gmai.com>
2015-01-26 15:01:08 +0800

    Merge branch 'master' of github.com:sunhaowen/PrimeMusic
    Conflicts:
    	tool/multiRequest.py

--- test.py
+++ test.py
@@ -3,13 +3,15 @@
 # author: sunhaowen@baidu.com
 # date: 2013/07/15 21:28
 
+from urlparse import urlparse
 
 def here():
     print("PrimeMusic")
 
 
 if __name__ == "__main__":
-    a = '1.23'
-    b = int(float(a))
-    print(b)
+    parsed_uri = urlparse( 'http://stackoverflow.com/questions/1234567/blah-blah-blah-blah' )
+    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    domain = parsed_uri.netloc
+    print domain
     here()
--- tool/MapReducer/mapper.py
+++ tool/MapReducer/mapper.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python
-# -*- coding:GBK
+# -*- coding:utf-8
 
 __author__ = 'sunhaowen'
 __date__ = '2015-01-23 10:51'
 
 import sys
+from urlparse import urlparse
 
 def here():
     print('PrimeMusic')
 
 
-def Mapper():
+def Mapper(movie_name_dict):
     """
     :return:
     """
     for line in sys.stdin:
-        line = line.decode('utf-8', 'ignore')
         line = line.strip()
         (query, total_search_count, total_click_count, total_satisfy_count, total_url_info) = line.split('\t', 4)
+        if not movie_name_dict.has_key(query):
+            continue
 
         while len(total_url_info):
             single_url_info_list = total_url_info.split('\t', 18)
             if len(single_url_info_list) < 18:
-                continue
-            if len(single_url_info_list) == 18:
+                break
+            if len(single_url_info_list) == 19:
                 total_url_info = single_url_info_list[-1]
             else:
                 total_url_info = ''
+
             url = single_url_info_list[0]
+            domain = urlparse(url).netloc
             title = single_url_info_list[1]
             search_count = int(single_url_info_list[2])
             click_count = int(single_url_info_list[5])
             satisfy_count = int(float(single_url_info_list[6]))
 
-            print('{0}\t{1}\t{2}\t{3}\t{4}\t{5}'.format(
-                query.encode('GBK', 'ignore'),
-                url.encode('GBK', 'ignore'),
-                title.encode('GBK', 'ignore'),
+            print('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}'.format(
+                query,
+                url,
+                domain,
+                title,
                 search_count,
                 click_count,
                 satisfy_count
@@ -44,6 +49,12 @@ def Mapper():
 
 
 if __name__ == '__main__':
+    movie_name_dict = {}
+    for line in open('./movieName.txt', 'r').readlines():
+        movie_name = line.strip()
+        movie_name_dict[movie_name] = 1
+
+    Mapper(movie_name_dict)
     here()    
 
 
